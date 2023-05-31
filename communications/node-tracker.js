87b8e370a1d024ef3b89bfc1ff6d25b489d41509
Hyun Park <hyunp.dev@gmail.com>
2023-06-01 03:46:56 +0900

    upload

--- communications/node-tracker.js
+++ communications/node-tracker.js
@@ -2,25 +2,6 @@ const http = require("http");
 const Peer = require("../datatypes/peer")
 const axios = require("axios");
 
-// /**
-//  *
-//  * @param {string} url address of node-tracker
-//  * @param {number} port port of node-tracker
-//  * @returns {Promise<[Peer]>} all peers what is turned on.
-//  */
-// function getPeers(url, port) {
-//     return new Promise((resolve, reject) => {
-//         http.get({
-//             hostname: url,
-//             port: port,
-//             path: '/nodes',
-//             agent: false,
-//         }, (res) => {
-//             resolve(JSON.parse(res).map(Peer.from))
-//         })
-//     })
-// }
-
 
 /**
  * a dummy function to test
