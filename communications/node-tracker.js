3a2526c83740a2149866c04f9516ea0f68ba72c8
Hyun Park <hyunp.dev@gmail.com>
2023-05-31 14:49:27 +0900

    upload

--- communications/node-tracker.js
+++ communications/node-tracker.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const Peer = require("../datatypes/peer")
+const axios = require("axios");
 
-/**
- * 
- * @param {string} url address of node-tracker
- * @param {number} port port of node-tracker
- * @returns {Promise<[Peer]>} all peers what is turned on.
- */
+// /**
+//  *
+//  * @param {string} url address of node-tracker
+//  * @param {number} port port of node-tracker
+//  * @returns {Promise<[Peer]>} all peers what is turned on.
+//  */
 // function getPeers(url, port) {
 //     return new Promise((resolve, reject) => {
 //         http.get({
@@ -20,20 +21,34 @@ const Peer = require("../datatypes/peer")
 //     })
 // }
 
-// 테스트용 더미 함수
-function getPeers(url, port) {
-    return new Promise((resolve, reject) => {
-                    resolve((
 
-                    [{
-                        address : "127.0.0.1",
-                        port : "11000"
-                    }]
+/**
+ * a dummy function to test
+ * @param {string} url address of node-tracker
+ * @param {number} port port of node-tracker
+ * @returns {Promise<[Peer]>} all peers what is turned on.
+ */
+async function getPeers(url, port) {
+    return [{
+        address : "127.0.0.1",
+        port : "11000"
+    }].map(Peer.from)
+}
 
-                    ).map(Peer.from))
-            })
+/**
+ *
+ * @param sensorID
+ * @returns {function(Peer): Promise<boolean>}
+ */
+function hasSensor(sensorID) {
+    return async function (peer) {
+        let url = `${peer.address}:${peer.port}/hasSensor?sensorID=${sensorID}`
+        let result = await axios.get(url)
+        return result.data === "true"
+    }
 }
 
 module.exports = {
-    getPeers: getPeers
+    getPeers: getPeers,
+    hasSensor: hasSensor
 }
--- routes/sensor.js
+++ routes/sensor.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const models = require('../models')
 const {assertBlockChain, SHA256} = require("../lib/blockchain");
+const {getPeers, hasSensor} = require("../communications/node-tracker");
 
 // const sequelize = require('sequelize')
 // const svalue = require('../models/svalue')(sequelize, sequelize.DataTypes)
@@ -39,8 +40,12 @@ router.post("/:sensorid/:valueid/:value", async function (req, res) {
         ),
         valueid: 1,
         value: 314,
-        beforehash: "null"
+        beforehash: beforehash
     })
+    let peers = await getPeers("127.0.0.1", 8000)
+    let hasResults = await Promise.all(peers.map(hasSensor(req.params.sensorid)))
+    let availablePeers = peers.filter((_, i) => hasResults[i])
+    availablePeers.map()
 })
 
 router.delete("/:sensorid", function (req, res) {
